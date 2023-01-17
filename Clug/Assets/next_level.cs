631f63c48041d01f5b86602f4d1bb0cd7a0f2862
santicleon1 <121570106+santicleon1@users.noreply.github.com>
2023-01-17 22:19:18 +0100

    Teleporting Player and Camera
    Successfully teleporting player and camera to next level

--- Clug/Assets/next_level.cs
+++ Clug/Assets/next_level.cs
@@ -1,15 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
- using System.Collections;
- 
- public class next_level : MonoBehaviour
+
+public class next_level : MonoBehaviour
 {
-    public GameObject portal;
+    public GameObject tpLvl2;
+    public GameObject cam;
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Portal")
+        if(other.gameObject.tag == "Portal")
         {
-            this.transform.position = portal.transform.position;
+            this.transform.position = tpLvl2.transform.position;
+            cam.transform.position = new Vector3(tpLvl2.transform.position.x, tpLvl2.transform.position.y);
         }
     }
 }
